da20c99827666a8f4a9a7485c99f77c98f9f6aea
Althaf Javed Singhawansa <73720067+AlthafSinghawansaUHN@users.noreply.github.com>
2023-02-02 14:34:26 -0500

    Add files via upload

--- launch.sh
+++ launch.sh
@@ -1,10 +1,11 @@
 #! /bin/bash -login
 #SBATCH -J snakemake-submission
-#SBATCH -t 7-00:00:00
+#SBATCH -t 5-00:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=althaf.singhawansa@uhnresearch.ca
 
 . env.sh
 
 echo 'Running on H4H cluster'
+snakemake -np --profile slurm --unlock
 snakemake -p --profile slurm
